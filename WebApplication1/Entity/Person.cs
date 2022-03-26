8e24c13635cf0554c973cb4d0e8f96b859cbdec0
phunjan <pritpalkaur05@gmail.com>
2022-03-26 17:15:50 +0800

    adding person

---
+++ WebApplication1/Entity/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Entity
+{
+    public class Person
+    {
+        public int pId { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+    }
+}
